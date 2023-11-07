445bf42ed7375c6ede9ef38c3f4b322b984ded08
sanarWH <vkhv0702@gmail.com>
2023-11-07 21:22:02 +0300

    KS-2-1 solution

---
+++ src/main/kotlin/lesson02_task01/task02.kt
@@ -0,0 +1,13 @@
+package lesson02_task01
+
+fun main(){
+
+    val numberOfStudents = 4
+    val students1 = 3f
+    val students2 = 4f
+    val students3 = 3f
+    val students4 = 5f
+
+    val gpa = (students1+students2+students3+students4) / numberOfStudents
+    println("Средний бал учеников с углубленным изучением английского языка: $gpa")
+}
