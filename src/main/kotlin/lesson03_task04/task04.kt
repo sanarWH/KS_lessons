d952b9ec8fcba96e690dd0f6d6e76590f1a2b715
sanarWH <vkhv0702@gmail.com>
2023-11-08 03:16:21 +0300

    KS-3-4 solution

---
+++ src/main/kotlin/lesson03_task04/task04.kt
@@ -0,0 +1,16 @@
+package lesson03_task04
+
+fun main() {
+    var from = "E2"
+    var move = "E4"
+    var numMove = 1
+
+    println("Игрок сделал ход из $from в $move. Номер хода: $numMove")
+
+    from = "D2"
+    move = "D3"
+    numMove  = 2
+
+    println("Игрок сделал ход из $from в $move. Номер хода: $numMove")
+
+}
