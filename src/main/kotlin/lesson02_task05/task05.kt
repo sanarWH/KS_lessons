3007731a801a5e224c193fbb499f27f8cd9bd2b8
sanarWH <vkhv0702@gmail.com>
2023-11-08 22:43:15 +0300

    KS-2-5 solution

--- src/main/kotlin/lesson02_task05/task05.kt
+++ src/main/kotlin/lesson02_task05/task05.kt
@@ -4,13 +4,12 @@ import kotlin.math.pow
 
 fun main() {
     val depositAmount = 70000
-    val percent = 0.167
+    val a = 0.167
+    val percentOfDeposit = 16.7
     val years = 20
+    val percent = percentOfDeposit / 100
 
-    val formulaFirstPart = 1 + percent
-    val formulaSecondPart = formulaFirstPart.pow(years)
-    val formulaThritPart = depositAmount * formulaSecondPart
-    val result = (String.format("%.3f", formulaThritPart))
+    val formula = (depositAmount * ((1 + percent).pow(years)))
+    val result = (String.format("%.3f", formula))
     println("Через 20 лет ваш вклад составит: $result рублей")
-
 }
