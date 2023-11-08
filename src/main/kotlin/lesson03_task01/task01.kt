2409b164c3f56095ba51e433a2d3ea785538328a
sanarWH <vkhv0702@gmail.com>
2023-11-08 21:16:23 +0300

    KS-3-1 solution

---
+++ src/main/kotlin/lesson03_task01/task01.kt
@@ -0,0 +1,10 @@
+package lesson03_task01
+
+fun main() {
+    val hiDay = "Добрый день!"
+    val hiNight = "Добрый вечер! "
+    val userName = "Иван"
+
+    println("$userName, $hiDay")
+    println("$userName, $hiNight")
+}
