2c11daece91bf9c7d2c3c1d5b76036afa286e21e
sanarWH <vkhv0702@gmail.com>
2023-11-09 18:57:11 +0300

    KS-3-4 solution

--- src/main/kotlin/lesson03_task04/task04.kt
+++ src/main/kotlin/lesson03_task04/task04.kt
@@ -3,14 +3,14 @@ package lesson03_task04
 fun main() {
     var from = "E2"
     var move = "E4"
-    var numMove = 1
+    var numMove = "1"
 
-    println("Игрок сделал ход из $from в $move. Номер хода: $numMove")
+    println("[$from-$move;$numMove]")
 
     from = "D2"
     move = "D3"
-    numMove  = 2
+    numMove  = "2"
 
-    println("Игрок сделал ход из $from в $move. Номер хода: $numMove")
+    println("[$from-$move;$numMove]")
 
 }
