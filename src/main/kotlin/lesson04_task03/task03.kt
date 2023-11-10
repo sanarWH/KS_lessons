e423b37f4d39a0ca1a231c83f631d8e77e4ceda2
sanarWH <vkhv0702@gmail.com>
2023-11-11 01:52:26 +0300

    KS-4-3 solution

--- src/main/kotlin/lesson04_task03/task03.kt
+++ src/main/kotlin/lesson04_task03/task03.kt
@@ -1,17 +1,17 @@
 package lesson04_task03
 
 fun main() {
-    val weather: Boolean = true
-    val awning = true
-    val wet = 20
-    val season = "Не зима"
 
-    val weatherNow = true //солнечная погода - true, любая другая - false
-    val awningNow = true //тент открыт - true, тент закрты - false
+    val isSunnyNow = true
+    val isAwningNowOpen = true
     val wetNow = 20
     val seasonNow ="Зима"
 
-    val conditionsSuitable = weatherNow == weather && awningNow == awning && wetNow == wet && seasonNow == season
+    val conditionsSuitable = isSunnyNow == ISWEATHERSUNNY && isAwningNowOpen == ISAWNINGOPEN && wetNow == WET && seasonNow == SEASON
 
     println("Благоприятные ли условия сейчас для роста бобовых? $conditionsSuitable")
 }
+const val ISWEATHERSUNNY: Boolean = true
+const val ISAWNINGOPEN = true
+const val WET = 20
+const val SEASON = "Не зима"
