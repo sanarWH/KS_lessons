3cc5dc59995b779314bcbf6353a27922be1e3553
sanarWH <vkhv0702@gmail.com>
2023-11-11 01:45:09 +0300

    KS-4-2 solution

--- src/main/kotlin/lesson04_task02/task02.kt
+++ src/main/kotlin/lesson04_task02/task02.kt
@@ -2,9 +2,6 @@ package lesson04_task02
 
 fun main(){
 
-    val averageWeightMin = 35
-    val averageWeightMax = 100
-    val averageVolume = 100
 
     var cargoWeight1 = 20
     var cargoVolume1 = 80
@@ -13,8 +10,11 @@ fun main(){
     var cargoVolume2 = 100
 
     println("Груз с весом: $cargoWeight1; и объемом: $cargoVolume1; соответствует категории Average: " +
-            "${cargoWeight1 >= averageWeightMin && cargoWeight1 <= averageWeightMax && cargoVolume1 <= averageVolume}")
+            "${cargoWeight1 >= AVERAGEWEIGHTMIN && cargoWeight1 <= AVERAGEWEIGHTMAX && cargoVolume1 < AVERAGEVOLUME}")
 
     println("Груз с весом: $cargoWeight2; и объемом: $cargoVolume2; соответствует категории Average: " +
-            "${cargoWeight2 >= averageWeightMin && cargoWeight2 <= averageWeightMax && cargoVolume2 <= averageVolume}")
+            "${cargoWeight2 >= AVERAGEWEIGHTMIN && cargoWeight2 <= AVERAGEWEIGHTMAX && cargoVolume2 < AVERAGEVOLUME}")
 }
+const val AVERAGEWEIGHTMIN = 35
+const val AVERAGEWEIGHTMAX = 100
+const val AVERAGEVOLUME = 100
