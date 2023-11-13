d9fdc28a36d079611fc22e4feb463defb1202fbb
sanarWH <vkhv0702@gmail.com>
2023-11-13 19:33:40 +0300

    KS-5-2 solution

--- src/main/kotlin/lesson04_task02/task02.kt
+++ src/main/kotlin/lesson04_task02/task02.kt
@@ -10,11 +10,11 @@ fun main(){
     var cargoVolume2 = 100
 
     println("Груз с весом: $cargoWeight1; и объемом: $cargoVolume1; соответствует категории Average: " +
-            "${cargoWeight1 >= AVERAG_EWEIGHT_MIN && cargoWeight1 <= AVERAGE_WEIGHT_MAX && cargoVolume1 < AVERAGE_VOLUME}")
+            "${cargoWeight1 >= AVERAGE_WEIGHT_MIN && cargoWeight1 <= AVERAGE_WEIGHT_MAX && cargoVolume1 < AVERAGE_VOLUME}")
 
     println("Груз с весом: $cargoWeight2; и объемом: $cargoVolume2; соответствует категории Average: " +
-            "${cargoWeight2 >= AVERAG_EWEIGHT_MIN && cargoWeight2 <= AVERAGE_WEIGHT_MAX && cargoVolume2 < AVERAGE_VOLUME}")
+            "${cargoWeight2 >= AVERAGE_WEIGHT_MIN && cargoWeight2 <= AVERAGE_WEIGHT_MAX && cargoVolume2 < AVERAGE_VOLUME}")
 }
-const val AVERAG_EWEIGHT_MIN = 35
+const val AVERAGE_WEIGHT_MIN = 35
 const val AVERAGE_WEIGHT_MAX = 100
 const val AVERAGE_VOLUME = 100
