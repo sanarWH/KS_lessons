1827c1267ebb5ffa52afa777905dc6fd65a809fb
sanarWH <vkhv0702@gmail.com>
2023-11-13 23:58:51 +0300

    KS-6-3 solution

---
+++ src/main/kotlin/lesson06_task03/task03.kt
@@ -0,0 +1,13 @@
+package lesson06_task03
+
+fun main() {
+    print("Введите количество секунд, которые нужно засечь: ")
+    val timer = readln().toInt()
+    var seconds = timer
+    while (seconds > 0){
+        println("Осталось секунд $seconds")
+        seconds--
+        Thread.sleep(1000)
+    }
+    println("Время вышло.")
+}
