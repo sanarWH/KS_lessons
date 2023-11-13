83dff341a8d4fb88ba9c8e7f74ce65f3a5d6f381
sanarWH <vkhv0702@gmail.com>
2023-11-13 23:47:04 +0300

    KS-6-2 solution

---
+++ src/main/kotlin/lesson06_task02/task02.kt
@@ -0,0 +1,16 @@
+package lesson06_task02
+
+import kotlin.concurrent.thread
+
+fun main() {
+    print("Введите количество секунд, которые нужно засечь: ")
+    val timer = readln().toInt()
+    var seconds = timer
+
+
+    while (seconds > 0){
+        seconds--
+        Thread.sleep(1000)
+    }
+    println("Прошло $timer секунд")
+}
