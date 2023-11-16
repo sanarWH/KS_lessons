b230ffb4f2ce8accf6b116a38e82797f35f0f0bc
sanarWH <vkhv0702@gmail.com>
2023-11-16 10:53:45 +0300

    KS-6-1 solution

--- src/main/kotlin/lesson06_task01/task01.kt
+++ src/main/kotlin/lesson06_task01/task01.kt
@@ -6,21 +6,12 @@ fun main() {
     print("Введите пароль: ")
     val createPassword = readln()
 
-    print("Окно авторизации. \nВведите ваш логин: ")
-    val userLogin = readln()
-    print("Введите ваш пароль:")
-    val userPassword = readln()
+    do {
+        print("Окно авторизации. \nВведите ваш логин: ")
+        val userLogin = readln()
+        print("Введите ваш пароль:")
+        val userPassword = readln()
+    } while (userLogin != createLogin && userPassword != createPassword)
 
-    while (userLogin == createLogin && userPassword == createPassword){
-        if(userLogin == createLogin && userPassword == createPassword){
-            println("Авторизация прошла успешно")
-            break
-        }else if (userLogin != createLogin || userPassword != createPassword){ //не понимаю, почему блок кода ниже не работает
-            println("Логин или пароль введены неверно. Попробуйте еще раз.")
-            print("Введите ваш логин: ")
-            val userLogin = readln()
-            print("Введите ваш пароль:")
-            val userPassword = readln()
-        }
-    }
+    println("Добро пожаловать!")
 }
