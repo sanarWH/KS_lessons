062dcbedfb45942a07c59ecc8364ee868b2e5a7e
sanarWH <vkhv0702@gmail.com>
2023-11-16 10:56:28 +0300

    KS-5-1 solution

--- src/main/kotlin/lesson05_taks01/task01.kt
+++ src/main/kotlin/lesson05_taks01/task01.kt
@@ -6,7 +6,7 @@ fun main() {
     val numberTwo = 2
     val solution = numberOne + numberTwo
     print("Докажите, что вы не робот. Решите пример и напишете ваш ответ: $numberOne + $numberTwo = ")
-    val solutionUser: Int = readln()!!.toInt()
+    val solutionUser: Int = readln().toInt()
 
     if(solutionUser == solution) println("Добро пожаловать!")
     else println("Доступ запрещен")
