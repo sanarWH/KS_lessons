1f4faf565f10ad9b0d4e75347f05e506fe45fc9d
sanarWH <vkhv0702@gmail.com>
2023-11-22 04:32:47 +0300

    KS-6-3 solution

--- src/main/kotlin/lesson06_task03/task03.kt
+++ src/main/kotlin/lesson06_task03/task03.kt
@@ -3,7 +3,7 @@ package lesson06_task03
 fun main() {
     print("Введите количество секунд, которые нужно засечь: ")
     var timer = readln().toInt()
-    while (timer > 0){
+    while (timer > 0) {
         println("Осталось секунд $timer")
         timer--
         Thread.sleep(1000)
